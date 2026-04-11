2606e484c8e74938affb5e5837669e27de369f8f
dodo5517 <kh9015548@gmail.com>
2026-04-11 14:07:51 +0900

    feat: BookWithLastRecordResponse에 year, pinned 추가

--- src/main/java/me/dodo/readingnotes/dto/book/BookWithLastRecordResponse.java
+++ src/main/java/me/dodo/readingnotes/dto/book/BookWithLastRecordResponse.java
@@ -4,14 +4,20 @@ import java.time.LocalDateTime;
 
 public class BookWithLastRecordResponse extends BookResponse {
     private LocalDateTime lastRecordAt;
+    private Integer year;
+    private boolean pinned;
 
     public BookWithLastRecordResponse(
             Long bookId, String title, String author, String isbn10,
-            String isbn13, String coverUrl, LocalDateTime lastRecordAt) {
+            String isbn13, String coverUrl, LocalDateTime lastRecordAt,
+            Integer year, boolean pinned) {
         super(bookId, title, author, isbn10, isbn13, coverUrl);
         this.lastRecordAt = lastRecordAt;
-
+        this.year = year;
+        this.pinned = pinned;
     }
 
     public LocalDateTime getLastRecordAt() { return lastRecordAt; }
+    public Integer getYear() { return year; }
+    public boolean isPinned() { return pinned; }
 }
