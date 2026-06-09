a97f074b6d04f8e11d84a7ee57e522c49299e504
dodo5517 <kh9015548@gmail.com>
2026-06-09 09:32:56 +0900

    refactor: cookie.secure 플래그를 application.properties로 외부화

--- src/main/java/me/dodo/readingnotes/util/CookieUtil.java
+++ src/main/java/me/dodo/readingnotes/util/CookieUtil.java
@@ -1,12 +1,17 @@
 package me.dodo.readingnotes.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 
 public class CookieUtil {
-    public static ResponseCookie createRefreshTokenCookie(String refreshToken, boolean isSecure) {
+
+    @Value("${cookie.secure}")
+    static boolean cookieSecure;
+
+    public static ResponseCookie createRefreshTokenCookie(String refreshToken) {
         return ResponseCookie.from("refreshToken", refreshToken)
                 .httpOnly(true)
-                .secure(isSecure)  // 운영 시에는 true로 해야 함
+                .secure(cookieSecure)  // 운영 시에는 true로 해야 함
                 .path("/") // 모든 경로에 쿠키 전송
                 .maxAge(7 * 24 * 60 * 60) // 7일
                 .sameSite("Strict") // CSRF 방지
@@ -16,7 +21,7 @@ public class CookieUtil {
     public static ResponseCookie deleteRefreshTokenCookie() {
         return ResponseCookie.from("refreshToken", "")
                 .httpOnly(true)
-                .secure(true)
+                .secure(cookieSecure)
                 .path("/")
                 .maxAge(0)
                 .sameSite("Strict")
